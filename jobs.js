edde325c1af438a20589933930afb5ee75984f11
Locklearjt95 <locklearjt95@gmail.com>
2026-09-24 20:11:01 -0400

    Create jobs.js

---
+++ jobs.js
@@ -0,0 +1,26 @@
+const jobs = [
+  {
+    company: "Example Transport",
+    title: "CDL-A Local Driver",
+    location: "Laurinburg, NC",
+    distance: 5,
+    homeDaily: true,
+    weeklyPay: 1500,
+    schedule: "Monday-Friday",
+    startTime: "11:00 PM",
+    equipment: "Day Cab",
+    freight: "Dry Van"
+  },
+  {
+    company: "Example Tanker",
+    title: "CDL-A Tanker Driver",
+    location: "Fayetteville, NC",
+    distance: 45,
+    homeDaily: true,
+    weeklyPay: 1800,
+    schedule: "5 days/week",
+    startTime: "Varies",
+    equipment: "Day Cab",
+    freight: "Tanker"
+  }
+];
